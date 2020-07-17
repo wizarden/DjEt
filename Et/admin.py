a2d732c17193332eadeafd17e0417d5f083f30e3
asrock <asrock@DENIS>
2020-07-17 20:48:21 +0300

    manytomany 2

--- Et/admin.py
+++ Et/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Izd
+from .models import Harak
+
+admin.site.register(Izd)
+
+admin.site.register(Harak)
+
--- Et/models.py
+++ Et/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 
+class Harak(models.Model):
+    color_cod       = models.CharField(max_length=4, default='9999')
+    color_name      = models.CharField(max_length=100, default='Название')
+    color_des       = models.CharField(max_length=100, default='')
+    
 
+    class Meta:
+        ordering = ['color_name']
+
+    def __str__(self):
+        return self.color_name
+        
 class Izd(models.Model):
     izd_cod       = models.CharField(max_length=4, default='9999')
     izd_name      = models.CharField(max_length=100, default='Название')
     izd_des       = models.CharField(max_length=100, default='')
+    haraks = models.ManyToManyField(Harak)
 
     class Meta:
         ordering = ['izd_name']
@@ -13,17 +25,7 @@ class Izd(models.Model):
         return self.izd_name
 
 
-class Harak(models.Model):
-    color_cod       = models.CharField(max_length=4, default='9999')
-    color_name      = models.CharField(max_length=100, default='Название')
-    color_des       = models.CharField(max_length=100, default='')
-    izds = models.ManyToManyField(Izd)
 
-    class Meta:
-        ordering = ['color_name']
-
-    def __str__(self):
-        return self.color_name
     
 
 # class Choice(models.Model):
