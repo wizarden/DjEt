7c02481d59e2bde938f447f481d68877bb242d7d
asrock <asrock@DENIS>
2020-07-17 20:21:34 +0300

    manytomany begin

--- DjEt/urls.py
+++ DjEt/urls.py
@@ -19,4 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
+    path('et/', include('Et.urls')),
+
 ]
---
+++ Et/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class Izd(models.Model):
+    izd_cod       = models.CharField(max_length=4, default='9999')
+    izd_name      = models.CharField(max_length=100, default='Название')
+    izd_des       = models.CharField(max_length=100, default='')
+
+    class Meta:
+        ordering = ['izd_name']
+
+    def __str__(self):
+        return self.izd_name
+
+
+class Harak(models.Model):
+    color_cod       = models.CharField(max_length=4, default='9999')
+    color_name      = models.CharField(max_length=100, default='Название')
+    color_des       = models.CharField(max_length=100, default='')
+    izds = models.ManyToManyField(Izd)
+
+    class Meta:
+        ordering = ['color_name']
+
+    def __str__(self):
+        return self.color_name
+    
+
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice_text = models.CharField(max_length=200)
+#     votes = models.IntegerField(default=0)
